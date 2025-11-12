91f539b1cb844d3b4c1f67263f8bf4bc5af7aa87
Kacper9829 <kacper1998m@gmail.com>
2025-11-12 10:32:31 +0100

    Delete Expense Tracker CS50 final project directory

--- Expense Tracker CS50 final project/expense_tracker/manage.py
+++
@@ -1,9 +0,0 @@
-from app import create_app, db 
-from flask_migrate import Migrate
-
-app = create_app()
-#responsible for data migration and database updates
-migrate = Migrate(app, db)
-
-if __name__ == "__main__":
-    app.run(debug=True)
